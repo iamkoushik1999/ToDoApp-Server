40594392f006f3aa35e0cebe5567b85bf92c9454
KoushikDutta1999 <iamkoushik1999@gmail.com>
2023-10-07 22:23:44 +0530

    Route - All Routes Added

---
+++ routes/todoRoutes.js
@@ -0,0 +1,27 @@
+const router = require("express").Router();
+// Controllers
+const {
+  todoList,
+  createToDo,
+  updateToDo,
+  deleteToDo,
+} = require("../controllers/todoCOntroller");
+
+// ----------------------------------------------------------- To Do -----------------------------------------------------------
+// GET
+// All TO DO LIST
+router.route("/list").get(todoList);
+
+// POST
+// CREATE TO DO
+router.route("/create").post(createToDo);
+
+// PUT
+// UPDATE TO DO
+router.route("/update/:id").put(updateToDo);
+
+// DELETE
+// DELETE TO DO
+router.route("/delete/:id").delete(deleteToDo);
+
+module.exports = router;
